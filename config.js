60b19eb5648f81f1973a20c3fb3d1152cfca5880
Pakkapon Phongthawee <pure.gif@gmail.com>
2015-03-18 14:43:56 +0700

    test

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 var server = {};
 server.api = "https://instagram.pureapp.in.th/";
 server.app = "purelike";
+server.contact = "https://www.facebook.com/appmadebypure";
+server.saleinfo = [["100","1","50","1 ปี"],["100","1","50","1 ปี"]];
