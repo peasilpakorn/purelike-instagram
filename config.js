f40118fdbeebda21a81707642b4908016c4beaa9
peasilpakorn <u07550103@su.ac.th>
2015-03-18 21:20:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var server = {};
 server.api = "https://instagram.pureapp.in.th/";
-server.app = "purelike";
-server.reseller = "IRIN";
-server.contact = "https://www.facebook.com/appmadebypure";
+server.app = "likekuyme";
+server.reseller = "mumu";
+server.contact = "https://www.facebook.com/mumutalk";
 server.saleinfo = [ ["30","10","ฟรี","ตลอดชีพ"], ["50","8","50","7 วัน"], ["70","5","100","15 วัน"], ["100","3","150","30 วัน"] ];
